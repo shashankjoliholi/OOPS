775cbb8ddd1b54962492d1df97c419071258e375
shashankjoliholi <163816984+shashankjoliholi@users.noreply.github.com>
2024-03-18 11:44:46 +0530

    Add files via upload

---
+++ pre and post .cpp
@@ -0,0 +1,16 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    cout<<"Pre increment = "<<++a<<endl;
+    int b=6;
+    cout<<"Post increment = "<<b++<<endl;
+    int c=2;
+    cout<<"Pre decrement = "<<--c<<endl;
+    int d=8;
+    cout<<"Post decrement = "<<d--<<endl;
+
+}
+
